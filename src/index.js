9b1efc0718060c78f1652587aedea28e5e48d3e6
Lika <borisovalika2@mail.com>
2020-09-29 14:40:41 +0300

    feat: towel-sort solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
+// jshint esversion:6
 
-// You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!Array.isArray(matrix) || matrix.length === 0) return [];
+
+  let newArr = [];
+  for (let i = 0; i <= matrix.length - 1; i++) {
+  	if (i % 2 === 0) {
+  		newArr.push(matrix[i]);
+  	} else if (i % 2)
+  	newArr.push(matrix[i].reverse());
+  }
+  return resultArray(newArr);
+};
+
+function resultArray(array) {
+	let result = [];
+	array.forEach(item => {
+  	item.forEach(el => {
+  		result.push(el);
+  	});
+  });
+  return result;
 }
